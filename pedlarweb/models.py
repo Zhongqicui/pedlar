837466d810bd3506921a019ca5d14e791ba1a5da
nuric <nuric@users.noreply.github.com>
2018-09-20 20:36:51 +0100

    Added Order table to database

--- pedlarweb/models.py
+++ pedlarweb/models.py
@@ -1,4 +1,5 @@
 """pedlarweb data models."""
+import datetime
 from . import bcrypt, db, login_manager, app
 
 
@@ -7,6 +8,10 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   username = db.Column(db.String(128), unique=True, nullable=False)
   _password = db.Column("password", db.String(128), nullable=False)
+  isadmin = db.Column(db.Boolean(), nullable=False, default=False)
+  orders = db.relationship('Order', cascade='all,delete', backref='user', lazy=True)
+  last_login = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.now())
+  joined = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.now())
 
   @property
   def password(self):
@@ -38,6 +43,18 @@ class User(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(user_id)
+
+
+class Order(db.Model):
+  """Single trade order."""
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  type = db.Column(db.String(8), nullable=False)
+  price_open = db.Column(db.Float(), nullable=False)
+  price_close = db.Column(db.Float())
+  profit = db.Column(db.Float())
+  closed = db.Column(db.DateTime())
+  created = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.now())
 
 # Check for in memory database
 if app.config['SQLALCHEMY_DATABASE_URI'] == "sqlite://":
--- pedlarweb/views.py
+++ pedlarweb/views.py
@@ -1,10 +1,12 @@
 """Endpoints for the web application."""
+import datetime
+
 from flask import render_template, redirect, url_for, request, jsonify
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 from . import app, db, broker
 from .forms import UserPasswordForm
-from .models import User
+from .models import User, Order
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,6 +18,8 @@ def login():
     if user:
       if user.is_correct_password(form.password.data):
         login_user(user)
+        user.last_login = datetime.datetime.now()
+        db.session.commit()
         return redirect(url_for('index'))
       else:
         return redirect(url_for('login'))
@@ -25,6 +29,7 @@ def login():
       db.session.add(user)
       db.session.commit()
       login_user(user)
+      app.logger.info("New user: %s", user.username)
       return redirect(url_for('index'))
   return render_template('login.html', form=form)
 
@@ -39,7 +44,22 @@ def trade():
   # Pass the trade request to broker
   req = request.json
   agent_name = req.pop('name', 'nobody')
-  return jsonify(broker.handle(req))
+  resp = broker.handle(req)
+  if resp['retcode'] == 0 and req['action'] in (2, 3):
+    # Record the new order
+    order = Order(id=resp['order_id'], user_id=current_user.id,
+                  type="BUY" if req['action'] == 2 else "SELL",
+                  price_open=resp['price'])
+    db.session.add(order)
+    db.session.commit()
+  elif resp['retcode'] == 0 and req['action'] == 1:
+    # Close the recorded order
+    order = Order.query.get(req['order_id'])
+    order.price_close = resp['price']
+    order.profit = resp['profit']
+    order.closed = datetime.datetime.now()
+    db.session.commit()
+  return jsonify(resp)
 
 @app.route('/logout')
 def logout():
