bb6b437c96b8dd43b87263674dc542a6bf750e58
nuric <nuric@users.noreply.github.com>
2018-10-06 23:11:27 +0100

    Changed username field requirements, added error messages to login form

--- pedlarweb/forms.py
+++ pedlarweb/forms.py
@@ -1,10 +1,16 @@
 """Web forms for pedlarweb."""
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Regexp, Length
 
 
 class UserPasswordForm(FlaskForm):
   """Username password form used for login."""
-  username = StringField('User or Team name', validators=[DataRequired(), Length(min=4, max=128)])
-  password = PasswordField('Password', validators=[DataRequired()])
+  # \w is [0-9a-zA-Z_]
+  username = StringField('User or Team name',
+                         validators=[DataRequired(),
+                                     Regexp(r"^\w(\w| )*\w$",
+                                            message="At least 2 alphanumeric characters with only spaces in between.")
+                                    ]
+                         )
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
