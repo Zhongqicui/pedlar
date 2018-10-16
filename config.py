129059ac24afccadc8ad36152e394b00ad494a65
nuric <nuric@users.noreply.github.com>
2018-10-16 10:51:24 +0100

    Added Google analytics for pedlarweb

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """pedlarweb configuration parameters."""
 DEBUG = True # Turns on debugging for Flask
-VERSION = "0.0.5" # Version of pedlar
+VERSION = "0.0.6" # Version of pedlar
 SECRET_KEY = "secretmaster3000" # Secret key for sessions
 
 BCRYPT_LOG_ROUNDS = 12 # Number of encryption rounds
@@ -13,6 +13,7 @@ BROKER_TIMEOUT = 4000 # Milliseconds to wait for response
 BROKER_LINGER = 2000 # Milliseconds to wait for closing broker socket
 TICKER_URL = "tcp://localhost:7000" # Ticker tcp endpoint
 
+GOOGLE_ANALYTICS = "" # GA Code UA-###
 LEADERBOARD_SIZE = 10 # Displays top N users
 RECENT_ORDERS_SIZE = 30 # Displays N most recent orders
 TICK_HIST_SIZE = 40 # Number ticks in tick chart
